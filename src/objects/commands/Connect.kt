23e6b91be577b16fe218c534268029e4f808e95a
AbelAnderson <Asatheabel@gmail.com>
2019-07-06 07:01:26 -0400

    Fixed Connect's Return Value

--- src/objects/commands/Connect.kt
+++ src/objects/commands/Connect.kt
@@ -4,16 +4,20 @@ import objects.Command
 import objects.Error
 import objects.GameState
 
-class Connect: Command("connect", "connect [IpAddress]: Connect to a different computer.") {
+class Connect : Command("connect", "connect [IpAddress]: Connect to a different computer.") {
     override fun execute(gameState: GameState, userCommand: String): String {
 
-        for(computer in gameState.activeComputers) {
-            if(computer.ipAddress == userCommand) {
+        var check = false
+
+        for (computer in gameState.activeComputers) {
+            if (computer.ipAddress == userCommand) {
                 gameState.currentComputer = computer
+                check = true
                 break
-            } else if(computer == gameState.activeComputers[gameState.activeComputers.lastIndex]) Error().objectNotFound(userCommand)
+            }
         }
 
-        return "Connected to $userCommand."
+        return if (check) "Connected to $userCommand."
+        else Error().objectNotFound(userCommand)
     }
 }
